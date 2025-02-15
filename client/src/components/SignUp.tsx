fd32d4cd4e2362996623a1b89819d8095c7ad873
Châu Thịnh <chauthinh00710@gmail.com>
2025-02-15 09:52:04 +0700

    Implement supabase for authentication

--- client/src/components/SignUp.tsx
+++ client/src/components/SignUp.tsx
@@ -1,19 +1,57 @@
 import React, { useState } from "react";
+import supabase from "../supabase";
+import { useNavigate } from "react-router-dom";
 import "./SignUp.css";
 
 const SignUp: React.FC = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [message, setMessage] = useState("");
+  const navigate = useNavigate();
 
-  const handleSubmit = (e: React.FormEvent) => {
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    console.log("Email:", email, "Password:", password);
+    setMessage("");
+
+    const { data, error } = await supabase.auth.signUp({
+      email: email,
+      password: password,
+    });
+
+    if (error) {
+      setMessage(error.message);
+      setEmail('');
+      setPassword('');
+      setUsername('');
+    } else {
+      setMessage("You're signed in successfully.");
+    }
+
+    if (data) {
+      setMessage("Created Account successfully.");
+      navigate('/');
+    }
+
+    setEmail('')
+    setPassword('')
   };
 
   return (
-    <div className="signin-container">
-      <form className="signin-form" onSubmit={handleSubmit}>
-        <h2>Welcome new member</h2>
+    <div className="signup-container">
+      <form className="signup-form" onSubmit={handleSubmit}>
+        <h2>Registration</h2>
+        <span>{message}</span>
+        <div className="input-group">
+          <label>Username</label>
+          <input
+            type="string"
+            placeholder="Display name"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
         <div className="input-group">
           <label>Email</label>
           <input
@@ -35,11 +73,12 @@ const SignUp: React.FC = () => {
           />
         </div>
         <div className="button-group">
-          <button type="submit" className="signin-button">
-            Sign In
-          </button>
-          <button type="button" className="signup-button" onClick={() => window.location.href = "/signup"}>
-            Sign Up
+          <button
+            type="button"
+            className="signup-button"
+            onClick={() => (window.location.href = "/signup")}
+          >
+            Sign Up with Your Email
           </button>
         </div>
       </form>

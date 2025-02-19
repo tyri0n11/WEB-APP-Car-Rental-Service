88460048f4d1e38c2c95d232054b3a856b0a8707
Châu Thịnh <chauthinh00710@gmail.com>
2025-02-19 20:58:06 +0700

    fix layouts

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,26 +4,31 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
 import Home from "./components/Home"; //
-import Banner from "./components/layout/Banner";
+// import Banner from "./components/layout/Banner";
 import Dashboard from "./components/Dashboard";
 import ProtectedRoute from "./routes/ProtectedRoute";
 import Profile from "./components/Profile";
 function App() {
   return (
     <BrowserRouter>
-      <Banner />
-      <NavBar />
-      <div className="content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
-          
-          <Route element={<ProtectedRoute />}>
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/profile" element={<Profile />} />
-          </Route>
-        </Routes>
+      <div className="wrapper">
+        <section>
+          {/* <Banner /> */}
+          <NavBar />
+        </section>
+
+        <section className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
+
+            <Route element={<ProtectedRoute />}>
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/profile" element={<Profile />} />
+            </Route>
+          </Routes>
+        </section>
       </div>
     </BrowserRouter>
   );
--- client/src/components/layout/Navbar.tsx
+++ client/src/components/layout/Navbar.tsx
@@ -1,10 +1,8 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { Search } from "lucide-react";
 import styles from "./Navbar.module.css";
 import { useAuth } from "../../context/AuthContext";
 import UserMenu from "../menu/UserMenu";
-
 
 const Navbar: React.FC = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -20,13 +18,11 @@ const Navbar: React.FC = () => {
         <div className={styles.logo}>
           <Link to="/">WAP</Link>
         </div>
-
-        <ul className={`${styles.navLinks} ${isMenuOpen ? styles.open : ""}`}>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
-        </ul>
-        <button className={styles.menuToggle} onClick={toggleMenu} aria-label="Toggle menu">
+        <button
+          className={styles.menuToggle}
+          onClick={toggleMenu}
+          aria-label="Toggle menu"
+        >
           <span className={isMenuOpen ? styles.barOpen : ""}></span>
           <span className={isMenuOpen ? styles.barOpen : ""}></span>
           <span className={isMenuOpen ? styles.barOpen : ""}></span>
@@ -34,23 +30,31 @@ const Navbar: React.FC = () => {
       </div>
 
       <div className={`${styles.menuItems} ${isMenuOpen ? styles.open : ""}`}>
-        <div className={styles.searchBar}>
-          <input type="text" placeholder="Search..." aria-label="Search" />
-          <button type="button" aria-label="Submit search">
-            <Search size={16} />
-          </button>
-        </div>
+        <ul className={`${styles.navLinks} ${isMenuOpen ? styles.open : ""}`}>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
         <div className={styles.authButtons}>
-        {user ? (
+          {user ? (
             <UserMenu />
           ) : (
             <>
-              <Link to="/signin" className={styles.loginButton}>Log In</Link>
-              <Link to="/signup" className={styles.signupButton}>Sign Up</Link>
+              <Link to="/signin" className={styles.loginButton}>
+                Log In
+              </Link>
+              <Link to="/signup" className={styles.signupButton}>
+                Sign Up
+              </Link>
             </>
           )}
         </div>
-        
       </div>
     </nav>
   );

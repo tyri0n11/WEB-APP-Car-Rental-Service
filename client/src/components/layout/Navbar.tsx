94a0a0a515e282d99d00e52c7b32d3c3f83d6cfa
thanhtrucsss <lnttrucs@gmail.com>
2025-03-15 10:40:08 +0700

    update home

--- client/src/components/layout/Navbar.tsx
+++ client/src/components/layout/Navbar.tsx
@@ -1,23 +1,15 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useAuth } from "../../hooks/useAuth";
 import styles from "./Navbar.module.css";
 
-const Navbar: React.FC<{
-  onSignInClick: () => void;
-  onSignUpClick: () => void;
-}> = ({ onSignInClick, onSignUpClick }) => {
+const Navbar: React.FC<{ onSignInClick: () => void; onSignUpClick: () => void }> = ({ onSignInClick, onSignUpClick }) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const navigate = useNavigate();
-  const { user, logout } = useAuth(); // Get user and logout function
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
-  const handleLogout = () => {
-    logout();
-    setIsMenuOpen(false);
-  };
+
   const handleNavigation = (path: string) => {
     navigate(path);
     setIsMenuOpen(false);
@@ -33,7 +25,7 @@ const Navbar: React.FC<{
           tabIndex={0}
           onKeyPress={(e) => e.key === "Enter" && handleNavigation("/")}
         >
-          <img src="/logo.png" width={100} height={50} alt="Logo" />
+          <img src="/logo.png" width={100} height={50} />
         </div>
         <button
           className={styles.menuToggle}
@@ -45,87 +37,70 @@ const Navbar: React.FC<{
           <span className={isMenuOpen ? styles.barOpen : ""}></span>
         </button>
         <div className={`${styles.menuItems} ${isMenuOpen ? styles.open : ""}`}>
-          <ul className={`${styles.navLinks} ${isMenuOpen ? styles.open : ""}`}>
-            <li>
-              <button
-                type="button"
-                className={styles.navButton}
-                onClick={() => handleNavigation("/about")}
-              >
-                About
-              </button>
-            </li>
-            <li>
-              <button
-                type="button"
-                className={styles.navButton}
-                onClick={() => handleNavigation("/services")}
-              >
-                Services
-              </button>
-            </li>
-            <li>
-              <button
-                type="button"
-                className={styles.navButton}
-                onClick={() => handleNavigation("/contact")}
-              >
-                Contact
-              </button>
-            </li>
-          </ul>
-        </div>
+  <ul className={`${styles.navLinks} ${isMenuOpen ? styles.open : ""}`}>
+    <li>
+      <button
+        type="button"
+        className={styles.navButton}
+        onClick={() => handleNavigation("/")}
+      >
+        Home
+      </button>
+    </li>
+    <li>
+      <button
+        type="button"
+        className={styles.navButton}
+        onClick={() => handleNavigation("/about")}
+      >
+        About
+      </button>
+    </li>
+    <li>
+      <button
+        type="button"
+        className={styles.navButton}
+        onClick={() => handleNavigation("/services")}
+      >
+        Services
+      </button>
+    </li>
+    <li>
+      <button
+        type="button"
+        className={styles.navButton}
+        onClick={() => handleNavigation("/contact")}
+      >
+        Contact
+      </button>
+    </li>
+  </ul>
+</div>
+
       </div>
 
-      {user ? (
-        <div className={styles.privateContainer}>
-          <div className={styles.dropdown}>
-            <button className={styles.dropdownButton}>
-              <img
-                className={styles.userAvatar}
-                src={"https://cdn-icons-png.flaticon.com/128/1077/1077012.png"}
-                alt="User Avatar"
-              />
-              <p className={styles.userGreet}>Welcome, {user.lastName}</p>
-            </button>
-            <div className={styles.dropdownContent}>
-              <button
-                type="button"
-                className={styles.dropdownItem}
-                onClick={() => handleNavigation("/profile")}
-              >
-                Profile
-              </button>
-              <button
-                type="button"
-                onClick={handleLogout}
-                className={styles.dropdownItemLogout}
-              >
-                Logout
-              </button>
-            </div>
-          </div>
-        </div>
-      ) : (
-        <div className={styles.authButtons}>
-          <button
-            type="button"
-            onClick={onSignInClick}
-            className={styles.loginButton}
-          >
-            Sign In
-          </button>
-          <button
-            type="button"
-            onClick={onSignUpClick}
-            className={styles.signupButton}
-          >
-            Sign Up
-          </button>
-        </div>
-      )}
+      
+
+      <div className={styles.authButtons}>
+        <button
+          type="button"
+          onClick={onSignInClick} 
+          className={styles.loginButton}
+        >
+          Sign In
+        </button>
+        <button
+          type="button"
+          onClick={onSignUpClick} 
+          className={styles.signupButton}
+        >
+          Sign Up
+        </button>
+      </div>
     </div>
   );
 };
+
+
 
 export default Navbar;

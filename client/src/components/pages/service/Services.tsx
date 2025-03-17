368071186af0146abf1d1eb3c4172103272f4bc4
ThinhChau <chauthinh00710@gmail.com>
2025-03-17 22:42:44 +0700

    Revert "Create filter bar + decorate ban"
    This reverts commit d2accbf713ec163e6cb29aee90393058c406b1a3.

--- client/src/components/pages/service/Services.tsx
+++ client/src/components/pages/service/Services.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import FilterBar from '../../filters/FilterBar';
 
 const Services: React.FC = () => {
   return (
       <main className="main-content">
-        <FilterBar onSearch={() => {}} onFilter={() => {}} />
+        {/* Introduction */}
+        <section className="introduction">
+          <h1>Our Rental Car Fleet</h1>
+          <p>
+            Lotus Car Rental is proud to offer unlimited mileage and all-inclusive rates on all rentals (insurance and taxes included in the price!). Save up to 20% by booking your rental car today!
+          </p>
+          <p>
+            Whether you’re cruising around the city of Reykjavík or exploring the backroads of Iceland, our fleet of 4x4 rental cars, vans, and compact car rentals provide safety, quality, and reliability during your Icelandic journey. Offering friendly and helpful customer service along the way, our cheap car rentals have unlimited mileage and no hidden costs. We have a great selection of 4x4 rental cars for F-road traveling during the summer period and winter driving in Iceland.
+          </p>
+        </section>
 
+        {/* Vehicle Categories */}
         <section className="vehicle-categories">
           <ul>
             <li><a href="#small-cars">Small Cars</a></li>
@@ -18,7 +27,9 @@ const Services: React.FC = () => {
           </ul>
         </section>
 
+        {/* Vehicle Listings */}
         <section className="vehicle-listings">
+          {/* Example Vehicle Listing */}
           <div className="vehicle-category" id="small-cars">
             <h2>Small Cars</h2>
             <div className="vehicle">
@@ -36,9 +47,11 @@ const Services: React.FC = () => {
                 <a href="/book/toyota-aygo" className="book-now">BOOK NOW</a>
               </div>
             </div>
+            {/* Additional vehicle listings can be added here following the same structure */}
           </div>
         </section>
 
+        {/* Included Benefits */}
         <section className="included-benefits">
           <h2>Included in our Rental Prices:</h2>
           <ul>
